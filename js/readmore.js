c6ab29800a6fd6c591c88151b3d6a4513373affb
Vivi Schulz <haweirh@gmail.com>
2025-03-28 14:58:41 -0700

    Attempt fix read more, change to lower case

---
+++ js/readmore.js
@@ -0,0 +1,19 @@
+
+      function toggleAll() {
+  var contents = document.querySelectorAll(".content");
+  var buttons = document.querySelectorAll(".read-more");
+
+  let isHidden = Array.from(contents).some(
+    (content) =>
+      content.style.display === "none" || content.style.display === ""
+  );
+
+  contents.forEach((content) => {
+    content.style.display = isHidden ? "block" : "none";
+  });
+
+  buttons.forEach((button) => {
+    button.textContent = isHidden ? "Show Less" : "Read More";
+  });
+}
+
