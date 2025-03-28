0d51ddd3668ee6b59214db2aff33ef260fede59f
Vivi Schulz <haweirh@gmail.com>
2025-03-28 14:35:58 -0700

    Attempt fix read more

--- js/readMore.js
+++ js/readMore.js
@@ -1,16 +1,17 @@
-             function toggleAll() {
-               var contents = document.querySelectorAll(".content");
-               var button = document.querySelector(".read-more");
+function toggleAll() {
+  var contents = document.querySelectorAll(".content");
+  var buttons = document.querySelectorAll(".read-more");
 
-               let isHidden = Array.from(contents).some(
-                 (content) =>
-                   content.style.display === "none" ||
-                   content.style.display === ""
-               );
+  let isHidden = Array.from(contents).some(
+    (content) =>
+      content.style.display === "none" || content.style.display === ""
+  );
 
-               contents.forEach((content) => {
-                 content.style.display = isHidden ? "block" : "none";
-               });
+  contents.forEach((content) => {
+    content.style.display = isHidden ? "block" : "none";
+  });
 
-               button.textContent = isHidden ? "Show Less" : "Read More";
-             }
+  buttons.forEach((button) => {
+    button.textContent = isHidden ? "Show Less" : "Read More";
+  });
+}
